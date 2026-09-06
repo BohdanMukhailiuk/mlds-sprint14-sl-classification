dbd319a6989041cb5a10add3be0fb3ad0208c0ab
Bohdan <bohdanmykhailiuk2001@gmail.com>
2026-09-06 23:24:50 +0300

    logistic-regression task

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -16,6 +16,7 @@ def read_csv(file_path):
     Returns:
     pd.DataFrame: Loaded data.
     """
+    data = pd.read_csv(file_path)
 
     return data
 
@@ -31,6 +32,8 @@ def train_logistic_regression(X, y):
     Returns:
     LogisticRegression: Trained logistic regression model.
     """
+    model = LogisticRegression(random_state=42)
+    model.fit(X, y)
 
     return model
 
@@ -46,6 +49,10 @@ def evaluate_model(y_true, y_pred):
     Returns:
     dict: Evaluation metrics (accuracy, precision, recall, F1-score).
     """
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average="weighted")
+    recall = recall_score(y_true, y_pred, average="weighted")
+    f1 = f1_score(y_true, y_pred, average="weighted")
 
     return {"Accuracy": accuracy, "Precision": precision, "Recall": recall, "F1-Score": f1}
 
